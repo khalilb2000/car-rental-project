586975bc4f909ec55d17ddaaa6483ac89d8f38a9
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2023-11-06 19:31:11 -0600

    commented out code and added navbar styles

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,37 +3,38 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from './App.jsx';
 import Home from './pages/Home';
-import Signup from './pages/Payment.jsx';
-import Login from './pages/Login';
-import SingleThought from './pages/About.jsx';
-import Profile from './pages/ProductInfo.jsx';
-import ErrorPage from './pages/location.jsx';
+// import Signup from './pages/Payment.jsx';
+// import Login from './pages/Login';
+// import SingleThought from './pages/About.jsx';
+// import Profile from './pages/ProductInfo.jsx';
+// import ErrorPage from './pages/location.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    errorElement: <ErrorPage />,
+    // errorElement: <ErrorPage />,
     children: [
       {
         index: true,
         element: <Home />
-      }, {
-        path: '/login',
-        element: <Login />
-      }, {
-        path: '/signup',
-        element: <Signup />
-      }, {
-        path: '/profiles/:username',
-        element: <Profile />
-      }, {
-        path: '/me',
-        element: <Profile />
-      }, {
-        path: '/thoughts/:thoughtId',
-        element: <SingleThought />
-      }
+      },
+      //
+      //   path: '/login',
+      //   element: <Login />
+      // }, {
+      //   path: '/signup',
+      //   element: <Signup />
+      // }, {
+      //   path: '/profiles/:username',
+      //   element: <Profile />
+      // }, {
+      //   path: '/me',
+      //   element: <Profile />
+      // }, {
+      //   path: '/thoughts/:thoughtId',
+      //   element: <SingleThought />
+      // }
     ]
   },
 ]);
