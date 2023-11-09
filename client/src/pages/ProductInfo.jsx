c132ad1e73e8cb49716764efc4f944955a540157
faysia1 <faysialopez@yahoo.com>
2023-11-08 18:03:36 -0600

    added product info page

--- client/src/pages/ProductInfo.jsx
+++ client/src/pages/ProductInfo.jsx
@@ -8,58 +8,59 @@
 
 // import Auth from '../utils/auth';
 
-// const Profile = () => {
-//   const { username: userParam } = useParams();
+const ProductInfo = () => {
+    // const { loading, data } = useQuery(QUERY_THOUGHTS);
+    // const thoughts = data?.thoughts || [];
+  
+    return (
+      <div className="select-a-car">
+        <div className="section">
+          <h2>Select One:</h2>
+        </div>
 
-//   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
-//     variables: { username: userParam },
-//   });
+      {/* show the sedan option */}
+        <div className="sedan">
+          <h3 className="card-header">Sedan</h3>
+          <img> </img>
+        </div>
+  
+        <button className="book-ride">Book Ride</button>
 
-//   const user = data?.me || data?.user || {};
-//   // navigate to personal profile page if username is yours
-//   if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
-//     return <Navigate to="/me" />;
-//   }
+        {/* show the truck option */}
+        <div className="truck">
+          <h3 className="card-header">Truck</h3>
+          <img> </img>
+        </div>
+  
+        <button className="book-ride">Book Ride</button>
 
-//   if (loading) {
-//     return <div>Loading...</div>;
-//   }
+        {/* show the van option */}
+        <div className="minivan">
+          <h3 className="card-header">Minivan</h3>
+          <img> </img>
+        </div>
+  
+        <button className="book-ride">Book Ride</button>
 
-//   if (!user?.username) {
-//     return (
-//       <h4>
-//         You need to be logged in to see this. Use the navigation links above to
-//         sign up or log in!
-//       </h4>
-//     );
-//   }
+        {/* show the sports car option */}
+        <div className="sports-car">
+          <h3 className="card-header">Sports Car</h3>
+          <img> </img>
+        </div>
+  
+        <button className="book-ride">Book Ride</button>
 
-//   return (
-//     <div>
-//       <div className="flex-row justify-center mb-3">
-//         <h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-5">
-//           Viewing {userParam ? `${user.username}'s` : 'your'} profile.
-//         </h2>
 
-//         <div className="col-12 col-md-10 mb-5">
-//           <ThoughtList
-//             thoughts={user.thoughts}
-//             title={`${user.username}'s thoughts...`}
-//             showTitle={false}
-//             showUsername={false}
-//           />
-//         </div>
-//         {!userParam && (
-//           <div
-//             className="col-12 col-md-10 mb-3 p-3"
-//             style={{ border: '1px dotted #1a1a1a' }}
-//           >
-//             <ThoughtForm />
-//           </div>
-//         )}
-//       </div>
-//     </div>
-//   );
-// };
+        {/* show the moving truck option */}
+        <div className="moving-truck">
+          <h3 className="card-header">Moving Truck</h3>
+          <img> </img>
+        </div>
+  
+        <button className="book-ride">Book Ride</button>
 
-// export default Profile;
+      </div>
+    );
+  }
+  
+  export default ProductInfo;
