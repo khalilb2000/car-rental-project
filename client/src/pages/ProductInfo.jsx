6d9e93cd78e6fb1714737d2a7ed282a5db539776
faysia1 <faysialopez@yahoo.com>
2023-11-09 19:57:28 -0600

    added functionality to page

--- client/src/pages/ProductInfo.jsx
+++ client/src/pages/ProductInfo.jsx
@@ -1,69 +1,9 @@
 // // import { Navigate, useParams } from 'react-router-dom';
 // // import { useQuery } from '@apollo/client';
 
-// // // import ThoughtForm from '../components/ThoughtForm';
-// // // import ThoughtList from '../components/ThoughtList';
-
 // // import { QUERY_USER, QUERY_ME } from '../utils/queries';
 
 // // import Auth from '../utils/auth';
-
-// const ProductInfo = () => {
-//     // const { loading, data } = useQuery(QUERY_THOUGHTS);
-//     // const thoughts = data?.thoughts || [];
-  
-//     return (
-//       <div className="select-a-car">
-//         <div className="section">
-//           <h2>Select One:</h2>
-//         </div>
-
-//       {/* show the sedan option */}
-//         <div className="sedan">
-//           <h3 className="card-header">Sedan</h3>
-//           {/* <img> </img> */}
-//         </div>
-  
-//         <button className="book-ride">Book Ride</button>
-
-//         {/* show the truck option */}
-//         <div className="truck">
-//           <h3 className="card-header">Truck</h3>
-//           {/* <img> </img> */}
-//         </div>
-  
-//         <button className="book-ride">Book Ride</button>
-
-//         {/* show the van option */}
-//         <div className="minivan">
-//           <h3 className="card-header">Minivan</h3>
-//           {/* <img> </img> */}
-//         </div>
-  
-//         <button className="book-ride">Book Ride</button>
-
-//         {/* show the sports car option */}
-//         <div className="sports-car">
-//           <h3 className="card-header">Sports Car</h3>
-//           {/* <img> </img> */}
-//         </div>
-  
-//         <button className="book-ride">Book Ride</button>
-
-
-//         {/* show the moving truck option */}
-//         <div className="moving-truck">
-//           <h3 className="card-header">Moving Truck</h3>
-//           {/* <img> </img> */}
-//         </div>
-  
-//         <button className="book-ride">Book Ride</button>
-
-//       </div>
-//     );
-//   }
-  
-//   export default ProductInfo;
 
 import { useState } from 'react';
 
@@ -97,31 +37,45 @@ const ProductInfo = () => {
       availability: true,
       image: './assets/chrysler-pacifica.png',
     },
-    // Add more vehicles as needed
+    {
+      id: 4,
+      brand: 'Lamborghini',
+      model: 'Huracan Tecnica',
+      year: 2023,
+      price: 200,
+      availability: false,
+      image: './assets/huracan-tecnica.png',
+    },
   ];
 
-  // const [expandedVehicle, setExpandedVehicle] = useState(null);
+  const [selectedVehicle, setSelectedVehicle] = useState(null);
 
-  // const handleClick = (vehicle) => {
-  //   if (expandedVehicle === vehicle) {
-  //     setExpandedVehicle(null); // Collapse if already expanded
-  //   } else {
-  //     setExpandedVehicle(vehicle); // Expand if not expanded
-  //   }
-  // };
+  const handleImageClick = (vehicle) => {
+    setSelectedVehicle((prevSelectedVehicle) => {
+      if (prevSelectedVehicle === vehicle.id) {
+        return null;
+      } else {
+        return vehicle.id;
+      }
+    });
+  };
 
   return (
     <div><h1>List of Available Vehicles</h1>
       {vehicles.map((vehicle) => (
-        <div key={vehicle.id}><h2>{vehicle.brand} {vehicle.model}</h2><img
+        <div key={vehicle.id}><h2>
+          {vehicle.brand} {vehicle.model}
+        </h2><img
             src={vehicle.image}
             alt={`${vehicle.brand} ${vehicle.model}`}
-            style={{ width: '200px', cursor: 'pointer' }}
-            // onClick={() => handleClick(vehicle)}
+            style={{ width: '200px' }}
+            onClick={() => handleImageClick(vehicle)}
           />
-          {/* {expandedVehicle === vehicle && ( */}
-            <div><p>Year: {vehicle.year}</p><p>Price per day: ${vehicle.price}</p><p>Availability: {vehicle.availability ? 'Available' : 'Not available'}</p></div>
-          {/* )} */}
+          {selectedVehicle === vehicle.id && (
+            <div><p>Year: {vehicle.year}</p><p>Price per day: ${vehicle.price}</p><p>
+              Availability: {vehicle.availability ? "Available" : "Not available"}
+            </p><button>Book Now!</button></div>
+          )}
           <hr /></div>
       ))}
     </div>
