fe962055fb15238540943b81f379a280022a27d8
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2023-11-15 13:40:11 -0600

    added finish page and switched buttn styles

--- client/src/pages/ProductInfo.jsx
+++ client/src/pages/ProductInfo.jsx
@@ -1,11 +1,20 @@
 // // import { Navigate, useParams } from 'react-router-dom';
 // // import { useQuery } from '@apollo/client';
+import { useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom'
+import ProgressBar from 'react-bootstrap/ProgressBar';
+import { Button, Container } from 'react-bootstrap';
 
 // // import { QUERY_USER, QUERY_ME } from '../utils/queries';
 
 // // import Auth from '../utils/auth';
 
-const ProductInfo = () => {
+const ProductInfo = (props) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  console.log(`${location.state.zipCode}`);
+
   const vehicles = [
     {
       id: 1,
@@ -64,20 +73,48 @@ const ProductInfo = () => {
   }
 
   return (
-    <div><h1 style={centerTextStyles}>List of Available Vehicles</h1>
-      {vehicles.map((vehicle) => (
-        <div key={vehicle.id}><h2>{vehicle.brand} {vehicle.model}</h2><div style={{ display: 'flex' }}><img
-          src={vehicle.image}
-          alt={`${vehicle.brand} ${vehicle.model}`}
-          onClick={() => handleImageClick(vehicle)}
-          style={{ cursor: 'pointer', marginRight: '20px', width: '225px' }}
-        />
-          {selectedVehicle === vehicle.id && (
-            <div><p>Year: {vehicle.year}</p><p>Price per day: ${vehicle.price}</p><p>Availability: {vehicle.availability ? 'Available' : 'Not available'}</p><button>Book Now!</button></div>
-          )}
-        </div><hr /></div>
-      ))}
-    </div>
+    <>
+      <Container className=''>
+        <h1>Step:2 </h1>
+        <ProgressBar animated now={66} />
+      </Container>
+      <Container>
+        <h1 style={centerTextStyles}>Select from our list of vehicles</h1>
+        <br/>
+
+        {vehicles.map((vehicle) => (
+          <Container key={vehicle.id}><h2>{vehicle.brand} {vehicle.model}</h2><Container style={{ display: 'flex' }}><img
+            src={vehicle.image}
+            alt={`${vehicle.brand} ${vehicle.model}`}
+            onClick={() => handleImageClick(vehicle)}
+            style={{ cursor: 'pointer', marginRight: '20px', width: '225px' }}
+          />
+            {selectedVehicle === vehicle.id && (
+              <Container>
+                <p>Year:{vehicle.year}</p>
+                <p>Price per day: ${vehicle.price}</p>
+                <p>Availability: {vehicle.availability ? 'Available' : 'Not available'}</p>
+                <Button
+                  onClick={() => {
+                    navigate('/finish',
+                      {
+                        replace: true,
+                        state: {
+                          zipCode: location.state.zipCode,
+                          pickupDate: location.state.pickupDate,
+                          dropOffDate: location.state.dropOffDate,
+                          selectedVehicle: vehicle,
+                        }
+                      })
+                  }}>
+                  Book Now!
+                </Button>
+              </Container>
+            )}
+          </Container><hr /></Container>
+        ))}
+      </Container>
+    </>
   );
 };
 
