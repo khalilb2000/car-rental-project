1831e395124c289a4a104f9c88c442d48b13e0a0
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2023-11-15 18:13:27 -0600

    added test commit for heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const password = process.env.clusterPW;
 const user = process.env.clusterUser
+///
 
 mongoose.connect(process.env.MONGODB_URI || `mongodb+srv://${user}:${password}@cluster1.th6nhgl.mongodb.net/carRentalDB`);
 
