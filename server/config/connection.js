a2d70dee4b59b980168e60d654300674419cb621
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2023-11-15 18:06:01 -0600

    initial heroku commit

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
+const password = process.env.clusterPW;
+const user = process.env.clusterUser
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/CarProject2023');
+mongoose.connect(process.env.MONGODB_URI || `mongodb+srv://${user}:${password}@cluster1.th6nhgl.mongodb.net/carRentalDB`);
 
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/car-rental');
 
 module.exports = mongoose.connection;
-
